0553a5b8f6da6e014c684e0fdfb2e582f0a95a25
clarkminor@g.ucla.edu <clarkminor@g.ucla.edu>
2017-03-11 11:53:33 -0800

    Initial Commit

---
+++ BruinNav/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BruinNav
+//
+//  Created by Clark Minor on 3/11/17.
+//  Copyright © 2017 Clark Minor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
